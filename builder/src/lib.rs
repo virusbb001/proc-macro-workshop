2f803f873fd33fc884a0acd902a8ddcd2101ce5c
virus <2017646+virusbb001@users.noreply.github.com>
2024-02-19 01:29:54 +0900

    complete builder 02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,36 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
+use syn::{self, parse_macro_input, DeriveInput, Data, Fields};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let ident = &ast.ident;
+    let Data::Struct(data) = ast.data else {
+        panic!("Builder is not used for struct");
+    };
+    let Fields::Named(_fields) = data.fields else {
+        panic!("Builder is not used for struct with named fields");
+    };
+    let builder_name = syn::Ident::new(&format!("{}Builder", ident), Span::call_site());
+    proc_macro::TokenStream::from(quote! {
+        pub struct #builder_name {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
 
-    TokenStream::new()
+        impl #ident {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    })
 }
