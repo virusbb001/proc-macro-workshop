ab98b954239987d6235aa2303cc0e56c27a88342
virus <2017646+virusbb001@users.noreply.github.com>
2024-02-29 01:01:09 +0900

    false-complete builder 02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,25 @@
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(_input: TokenStream) -> TokenStream {
-    TokenStream::new()
+    quote! {
+        pub struct CommandBuilder {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl Command {
+            pub fn builder() -> CommandBuilder {
+                CommandBuilder {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    }.into()
 }
