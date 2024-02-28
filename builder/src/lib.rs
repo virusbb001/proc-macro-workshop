662ee9136ca5ca7b45a645e5441e272f287c8c2e
virus <2017646+virusbb001@users.noreply.github.com>
2024-02-29 00:33:31 +0900

    complete builder 01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,6 @@
 use proc_macro::TokenStream;
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+pub fn derive(_input: TokenStream) -> TokenStream {
+    TokenStream::new()
 }
