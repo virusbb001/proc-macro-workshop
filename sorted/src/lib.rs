d8b2cdb81a18545a46cbcd8681bfc573df291163
virus <2017646+virusbb001@users.noreply.github.com>
2024-03-20 02:41:14 +0900

    complete sorted 02-not-enum

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,4 +1,5 @@
 use proc_macro::TokenStream;
+use proc_macro2::Span;
 use quote::ToTokens;
 use syn::parse_macro_input;
 
@@ -7,5 +8,9 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
 
     let item = parse_macro_input!(input as syn::Item);
-    item.to_token_stream().into()
+    if matches!(item, syn::Item::Enum(_)) {
+        item.to_token_stream().into()
+    } else {
+        syn::Error::new(Span::call_site(), "expected enum or match expression").to_compile_error().into()
+    }
 }
