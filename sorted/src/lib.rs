dd6b1a3fd9023333d3fbff35a19d8c1e99f26aa0
virus <2017646+virusbb001@users.noreply.github.com>
2024-03-20 02:26:59 +0900

    complete sorted 01

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,11 @@
 use proc_macro::TokenStream;
+use quote::ToTokens;
+use syn::parse_macro_input;
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
 
-    unimplemented!()
+    let item = parse_macro_input!(input as syn::Item);
+    item.to_token_stream().into()
 }
