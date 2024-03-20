e2d07140317935eb638c92dc0f0bd50d49377687
virus <2017646+virusbb001@users.noreply.github.com>
2024-03-21 00:43:34 +0900

    complete bitfield 02-storage

--- bitfield/impl/src/lib.rs
+++ bitfield/impl/src/lib.rs
@@ -1,9 +1,18 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemStruct};
 
 #[proc_macro_attribute]
 pub fn bitfield(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
+    let item_struct = parse_macro_input!(input as ItemStruct);
 
-    unimplemented!()
+    let field_tys = item_struct.fields.iter().map(|field| &field.ty);
+
+    quote! {
+        #[repr(C)]
+        pub struct MyFourBytes {
+            data: [u8; (0 #(+ #field_tys::BITS)* )/8],
+        }
+    }.into()
 }
