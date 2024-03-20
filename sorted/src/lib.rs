c4071e09d0a4598c545c98e9934788ba430b0038
virus <2017646+virusbb001@users.noreply.github.com>
2024-03-20 17:12:32 +0900

    complete sorted 05-match-expr

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -2,13 +2,27 @@ use proc_macro::TokenStream;
 use proc_macro2::Span;
 use proc_macro2::TokenStream as TokenStream2;
 use quote::ToTokens;
+use syn::ExprMatch;
+use syn::Ident;
+use syn::Pat;
 use syn::parse_macro_input;
+use syn::visit_mut;
+use syn::visit_mut::VisitMut;
 
 #[derive(Debug)]
 struct WrongLocations {
     target: String,
     expected: String,
     span: Span,
+}
+
+impl From<&WrongLocations> for syn::Error {
+    fn from (wrong: &WrongLocations) -> Self{
+        syn::Error::new(
+            wrong.span,
+            format!("{} should sort before {}", wrong.target, wrong.expected),
+        )
+    }
 }
 
 #[proc_macro_attribute]
@@ -21,27 +35,12 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
             .to_compile_error()
             .into()
     };
-    let mut sorted_variants = item_enum
+    let idents = item_enum
         .variants
         .iter()
         .map(|variant| &variant.ident)
-        .enumerate()
         .collect::<Vec<_>>();
-    sorted_variants.sort_by_key(|v| v.1);
-    let mut wrong_positions = sorted_variants
-        .iter()
-        .enumerate()
-        .filter_map(|(i, item)| {
-            sorted_variants
-                .iter()
-                .skip(i + 1)
-                .find(|v| v.0 < item.0)
-                .map(|other| WrongLocations {
-                    target: item.1.to_string(),
-                    expected: other.1.to_string(),
-                    span: item.1.span(),
-                })
-        })
+    let mut wrong_positions = get_unsorted_items(&idents).iter()
         .map(|wrong| {
             syn::Error::new(
                 wrong.span,
@@ -52,4 +51,60 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
         .collect::<TokenStream2>();
     wrong_positions.extend(item_enum.to_token_stream());
     wrong_positions.into()
+}
+
+struct SortedInFn(Vec<WrongLocations>);
+
+impl VisitMut for SortedInFn {
+    fn visit_expr_match_mut (&mut self, expr_match: &mut ExprMatch) {
+        let sorted_position = expr_match.attrs.iter().position(|v| {
+            v.meta.path().is_ident("sorted")
+        });
+        if let Some(sorted_index) = sorted_position {
+            expr_match.attrs.remove(sorted_index);
+            let idents = expr_match.arms.iter().filter_map(|arm| {
+                if let Pat::TupleStruct(tuple_struct) = &arm.pat {
+                    tuple_struct.path.segments.last().map(|segment| &segment.ident)
+                } else {
+                    None
+                }
+            }).collect::<Vec<_>>();
+            self.0.extend(get_unsorted_items(&idents));
+        }
+        visit_mut::visit_expr_match_mut(self, expr_match);
+    }
+}
+
+#[proc_macro_attribute]
+pub fn check(_: TokenStream, input: TokenStream) -> TokenStream {
+    let mut item = parse_macro_input!(input as syn::ItemFn);
+    let mut sorted_in_fn = SortedInFn(Vec::new());
+    sorted_in_fn.visit_item_fn_mut(&mut item);
+    let mut errors = sorted_in_fn.0
+        .iter()
+        .map(|wrong| syn::Error::from(wrong).to_compile_error())
+        .collect::<TokenStream2>();
+    errors.extend(item.to_token_stream());
+    errors.into()
+}
+
+fn get_unsorted_items(idents: &[&Ident]) -> Vec<WrongLocations> {
+    let mut idents = idents.iter().enumerate().collect::<Vec<_>>();
+    idents.sort_by_key(|v| v.1);
+
+    idents
+        .iter()
+        .enumerate()
+        .filter_map(|(i, item)| {
+            idents
+                .iter()
+                .skip(i + 1)
+                .find(|v| v.0 < item.0)
+                .map(|other| WrongLocations {
+                    target: item.1.to_string(),
+                    expected: other.1.to_string(),
+                    span: item.1.span(),
+                })
+        })
+        .collect::<Vec<_>>()
 }
